b9eaa90d00caf2af301e49cc4976971fdfc3a07c
ErickIbrahim18 <erickibrahim18@gmail.com>
2021-01-31 15:52:28 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam delectus dolores aperiam voluptas necessitatibus expedita esse saepe nemo nostrum. Accusantium provident recusandae saepe, ullam a error mollitia sint est aspernatur.</p>
 </body>
 
 </html>
